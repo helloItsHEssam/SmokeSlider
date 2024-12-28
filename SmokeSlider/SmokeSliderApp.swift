de0c64674341c00dc0259722a0bc9328a5c1f184
Hessam Mahdiabadi <h.mahdiabadi@bluteam.ir>
2024-12-28 18:39:04 +0330

    Initial Commit

---
+++ SmokeSlider/SmokeSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmokeSliderApp.swift
+//  SmokeSlider
+//
+//  Created by HEssam on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SmokeSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
